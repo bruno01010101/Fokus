2e12d54f1c2d6522d0cf64a68710b7af88197be6
Bruno dos santos <bruno16082006@gmail.com>
2026-06-17 16:43:39 -0300

    faltou algumas coisas rsrs

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const h1 = document.querySelector('app__title')
 const btn1 = document.querySelector('.app__card-button--foco')
 const btn2 = document.querySelector('.app__card-button--curto')
 const btn3 = document.querySelector('.app__card-button--longo')
+const botaoIniciar = document.querySelector('.app__card-primary-button');
+const temp_foco = 1500;
+const temp_curto = 300;
+const temp_longo = 900;
+
 
 document.addEventListener(('click'), (e) => {
     const botao = e.target;
