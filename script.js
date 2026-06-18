262186fa39950186fb6ca7682844b3381ad239e4
Bruno dos santos <bruno16082006@gmail.com>
2026-06-18 17:52:39 -0300

    sla

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const html = document.querySelector('html')
 const card = document.querySelector('.app__card');
 const img = document.querySelector('.app__image');
-const h1 = document.querySelector('app__title')
+const h1 = document.querySelector('.app__title')
+const strong = document.querySelector('.app__title-strong')
 const btn1 = document.querySelector('.app__card-button--foco')
 const btn2 = document.querySelector('.app__card-button--curto')
 const btn3 = document.querySelector('.app__card-button--longo')
@@ -14,21 +15,29 @@ const temp_longo = 900;
 document.addEventListener(('click'), (e) => {
     const botao = e.target;
     if(botao == btn1){
-        html.setAttribute('data-contexto', 'foco') // método que muda o atributo de uma tag
+        mudaAtributos('data-contexto', 'foco')
         btn2.classList.remove('active')
         btn3.classList.remove('active')
         botao.classList.add('active')
     }
     if(botao == btn2){
-        html.setAttribute('data-contexto', 'descanso-curto')
+        mudaAtributos('data-contexto', 'descanso-curto')
         btn1.classList.remove('active')
         btn3.classList.remove('active')
         botao.classList.add('active')
     }
     if(botao == btn3){
-        html.setAttribute('data-contexto', 'descanso-longo')
+        mudaAtributos('data-contexto', 'descanso-longo')
         btn2.classList.remove('active')
         btn1.classList.remove('active')
         botao.classList.add('active')
     }
 })
+
+function mudaAtributos(atributo, atribuicao){
+    html.setAttribute(atributo, atribuicao) // método que muda o atributo de uma tag
+    img.setAttribute('src', `/imagens/${atribuicao}.png`)
+    if (atribuicao === 'foco') h1.innerHTML = `Otimize sua produtividade,<br> <strong class="app__title-strong">Mergulhe no que importa.</strong>`;
+    if (atribuicao === 'descanso-curto') h1.innerHTML = `Que tal dar uma respirada?<br> <strong class="app__title-strong">Faça uma pausa curta.</strong>`;
+    if (atribuicao === 'descanso-longo') h1.innerHTML = `Hora de voltar a superfície<br> <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
+}
